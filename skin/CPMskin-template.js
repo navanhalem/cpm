e0fa40bfd89f9a7ab3f998e7b3fc6c0db857072d
Nino van Halem <na.vanhalem@gmail.com>
2018-10-13 18:24:02 +0200

    started adding chemotaxis to the epidermis browser simulation

--- skin/CPMskin-template.js
+++ skin/CPMskin-template.js
@@ -3,7 +3,7 @@ var Cset = {
 	field_size : {x:200,y:200},
 	conf : {LAMBDA_P : [0,2,2],
 		LAMBDA_V : [0,30,30],
-		LAMBDA_ACT : [0,20,0],
+		LAMBDA_ACT : [0,400,0],
 		MAX_ACT : [0,1000,0],
 		P : [0,125,145],
 		V : [0,100,145],
@@ -14,6 +14,30 @@ var Cset = {
 		ACT_MEAN : "geometric"
 	}
 }
+
+var CsetChemotaxis = {
+	ndim : 2,
+	field_size : {x:200,y:200},
+	conf : {LAMBDA_CHEMOTAXIS : [0,30,0],
+		LAMBDA_CONNECTIVITY : [0,0,0],
+		LAMBDA_P : [0,2,2],
+		LAMBDA_V : [0,30,30],
+		LAMBDA_ACT : [0,400,0],
+		MAX_ACT : [0,1000,0],
+		P : [0,125,145],
+		V : [0,100,145],
+		J_T_STROMA : [NaN,15,15],
+		J_T_ECM : [NaN,20,20],
+		J_T_T : [ [NaN,NaN,NaN], [NaN,100,20], [NaN,20,20] ],
+		T : 20,
+		ACT_MEAN : "geometric",
+		GRADIENT_TYPE : "radial",
+		GRADIENT_DIRECTION : [99,99]
+	}
+}
+
+
+
 
 if( typeof module !== "undefined" ){
 	module.exports = Cset
